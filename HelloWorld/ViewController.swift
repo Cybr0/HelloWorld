5fd8386f0bd5bb27023bd6470119aaf23a6776e7
Tim Cybr0 <tim.cybr0@gmail.com>
2022-02-21 01:46:56 +0500

    Add vertical stack view

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,22 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var hiWorldLabel: UILabel!
     @IBOutlet var toggleButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        helloWorldLabel.isHidden = true
+        hiWorldLabel.isHidden = true
         toggleButton.layer.cornerRadius = 10
     }
 
     @IBAction func makeButtonAction() {
-        if helloWorldLabel.isHidden {
-            helloWorldLabel.isHidden = false
+        if hiWorldLabel.isHidden {
+            hiWorldLabel.isHidden = false
             toggleButton.setTitle("Hide Text", for: .normal)
         } else {
-            helloWorldLabel.isHidden = true
+            hiWorldLabel.isHidden = true
             toggleButton.setTitle("Show Text", for: .normal)
         }
         
