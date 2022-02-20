d9388df2a3388fcad53a0ed5b1d17d8940cd888d
Tim Cybr0 <tim.cybr0@gmail.com>
2022-02-20 18:14:10 +0500

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Tim Yusupov on 2/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
